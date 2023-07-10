f6742026736b9dbb3cfdf8742c43ad969c3897ac
jaejae990921 <jaejae990921@naver.com>
2023-07-10 17:58:56 +0900

    prac : 실습 3 array

---
+++ HTML/230710/array.js
@@ -0,0 +1,15 @@
+let colors = ["red", "orange", "yellow", "green", "blue", "darkblue", "purple"];
+
+// 1
+console.log(colors[2]);
+
+// 2
+colors.push("aqua");
+console.log(colors);
+
+// 3
+console.log(colors.indexOf("black"));
+
+// 4
+colors.reverse();
+console.log(colors);
