3a7ae0ebd4af5d134c08b28b1f15f7ded12e7359
jaejae990921 <jaejae990921@naver.com>
2023-08-08 23:48:31 +0900

    study : MVC 패턴 공부
    기존 index.js에 모여있던 코드를 MVC 패턴으로 분리

--- Nodejs/230807_mvc_mysql/index.js
+++ Nodejs/230807_mvc_mysql/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
-const mysql = require('mysql');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -9,64 +8,19 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// mysql 연결
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'simson',
-    password: 'simson',
-    database: 'kdt9'
-})
-
-conn.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('mysql connected');
-});
-
 // const indexRouter = require('./routes');
 // app.use('/', indexRouter);
+
+const visitorRouter = require('./routes/visitor');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// GET /visitor -> 방명록 전체 보이기
-app.get('/visitor', (req, res) => {
-    const query = 'SELECT * FROM visitor';
-    conn.query(query, (err, rows) => {
-        console.log(rows);
-        res.render('visitor', { data: rows });
-    })
-});
-
-// GET /visitor/get -> 방명록 하나 조회
-app.get('/visitor/get', (req, res) => {
-    res.send('방명록 하나 조회');
-});
-
-// POST /visitor/write -> 방명록 하나 생성
-app.post('/visitor/write', (req, res) => {
-    const query = `INSERT INTO visitor (name, comment) VALUES ('${req.body.name}', '${req.body.comment}')`;
-    conn.query(query, (err, rows) => {
-        console.log('rows : ', rows);
-        res.send({id: rows.insertId, name: req.body.name, comment: req.body.comment});
-    });
-});
-
-// PATCH /visitor/edit -> 방명록 하나 수정
-app.patch('/visitor/edit', (req, res) => {
-    res.send('방명록 하나 수정');
-});
-
-// DELETE /visitor/delete -> 방명록 하나 삭제
-app.delete('/visitor/delete', (req, res) => {
-    res.send('방명록 하나 삭제');
-});
+app.use('/visitor', visitorRouter);
 
 app.use('*', (req, res) => {
-    res.render('404');
+    res.render('404');  
 });
 
 app.listen(PORT, () => {
