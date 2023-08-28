b950f82f5b39b5abd6b4ea75a3dfaa4fe82631a6
jaejae990921 <jaejae990921@naver.com>
2023-08-28 20:39:40 +0900

    study : Socket 기본 문법 공부
    [서버]
    소켓 생성 및 각 이벤트 생성 // 값 받고 보내기
    연결된 소켓 배열로 관리 등 공부
    
    [클라이언트]
    서버와의 연결, 값 보내고 받기 등

---
+++ Nodejs/230828_websocket/server.js
@@ -0,0 +1,46 @@
+const ws = require('ws');
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('client');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
+
+// 웹소켓 서버 접속
+const wss = new ws.Server({ /* port: PORT */ server });
+// 클라이언트들을 담을 배열
+const sockets = [];
+
+// 웹소켓 서버 이벤트
+// on은 이벤트를 등록하는 메서드
+// socket은 접속한 클라이언트
+wss.on('connection', (socket) => {
+    console.log('클라이언트 연결');
+    sockets.push(socket); // 배열에 추가
+
+    // 메세지 이벤트
+    socket.on('message', (message) => {
+        console.log(`클라이언트로부터 받은 메세지 : ${message}`);
+        // socket.send(`서버 : ${message}`);
+        sockets.forEach( elem => {
+            elem.send(`서버 : ${message}`);
+        })
+    });
+
+    // 오류 이벤트
+    socket.on('error', (err) => {
+        console.log(`에러가 발생했습니다. (에러:${err})`);
+    });
+
+    // 접속 종료 이벤트
+    socket.on('close', () => {
+        console.log('클라이언트 접속 종료');
+    });
+})  
