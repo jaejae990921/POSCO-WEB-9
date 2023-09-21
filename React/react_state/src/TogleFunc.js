2a15e632bce53c4f4cdc0ebcfcaf97d27acc72f9
jaejae990921 <jaejae990921@naver.com>
2023-09-21 20:44:16 +0900

    study : useState 공부

---
+++ React/react_state/src/TogleFunc.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export default function TogleFunc() {
+  const [status, setStatus] = useState(false);
+
+  const handleToggle = () => {
+    if (status === false) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={handleToggle}>토글</button>
+        <p>{status && '보여라'}</p>
+      </div>
+    </>
+  );
+}
