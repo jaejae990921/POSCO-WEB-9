17ca0d57448f969b54e3bf71b279f1565f7496d9
jaejae990921 <jaejae990921@naver.com>
2023-09-25 23:20:16 +0900

    prac : useReducer 실습
    오류가 발생하는 예제를 정상 작동되게 고쳐보는 실습
    Submit 버튼을 눌렀을 때, dispatch 값과 type의 이름이 맞지 않아서 이름 변경해서 해결

--- React/react_basic2/src/App.js
+++ React/react_basic2/src/App.js
@@ -1,9 +1,32 @@
 import LifeCycleFunc from './08Lifecycle/LifeCycleFunc';
+import RefSampleClass1 from './09Ref/RefSampleClass1';
+import RefSampleClass2 from './09Ref/RefSampleClass2';
+import RefSampleFunc1 from './09Ref/RefSampleFunc1';
+import RefSampleFunc2 from './09Ref/RefSampleFunc2';
+import RefSampleFunc3 from './09Ref/RefSampleFunc3';
+import UesMemo from './10Hook/useMemo'
+import UseCallback from './10Hook/useCallback'
+import UseReducer from './10Hook/useReducer'
 
 function App() {
   return (
     <>
-      <LifeCycleFunc />
+      {/* <LifeCycleFunc /> */}
+
+      {/* 09Ref */}
+      {/* <RefSampleClass1 /> */}
+      {/* <RefSampleClass1 /> */}
+      {/* <RefSampleClass2 /> */}
+      {/* <RefSampleClass2 /> */}
+
+      {/* <RefSampleFunc1 /> */}
+      {/* <RefSampleFunc2 /> */}
+      {/* <RefSampleFunc3 /> */}
+
+      {/* 10Hook */}
+      {/* <UesMemo /> */}
+      {/* <UseCallback /> */}
+      <UseReducer />
     </>
   );
 }
