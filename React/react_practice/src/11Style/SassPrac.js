12d21c2ed315d2d792732ad335c70350130f703f
jaejae990921 <jaejae990921@naver.com>
2023-09-26 23:55:23 +0900

    prac : Sass 실습 2
    회전하는 원 3개 만들기

---
+++ React/react_practice/src/11Style/SassPrac.js
@@ -0,0 +1,11 @@
+import './SassPrac.scss';
+
+export default function SassPrac() {
+  return (
+    <div>
+      <div className="circle pink"></div>
+      <div className="circle skyblue"></div>
+      <div className="circle green"></div>
+    </div>
+  );
+}
