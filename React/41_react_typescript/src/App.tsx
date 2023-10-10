03180a3d9abfd70bd4f44809f698e7419723f963
jaejae990921 <jaejae990921@naver.com>
2023-10-10 23:37:58 +0900

    study : typescript react 공부
    react에서 typescript를 사용하는 방법.
    props, useState, ref, interface 등 리액트와 타입스크립트 접목 방법 공부

---
+++ React/41_react_typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import Types from './Types';
+
+function App() {
+  return (
+    <>
+      <Types name="simson" />
+    </>
+  );
+}
+
+export default App;
---
+++ React/41_react_typescript/src/Types.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react';
+
+interface Props {
+  name?: string;
+}
+
+interface Data {
+  name: string;
+  age: number;
+}
+
+// 컴포넌트 제작 방법 2
+const Types: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<Data>();
+  // const [count, setCount] = useState<Data | Props>();
+  // const [count, setCount] = useState<Data | null>();
+  // 유연하게 사용 가능
+
+  const myInput = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <h2>Hello {props.name}</h2>
+      <input ref={myInput} />
+    </>
+  );
+};
+
+export default Types;
+
+// 컴포넌트 제작 방법 1
+// export default function Types({ name }: Props) {
+//   return (
+//     <>
+//       <h2>Hello {name}</h2>
+//     </>
+//   );
+// }
