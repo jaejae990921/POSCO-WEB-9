e1da97942561c9e7c0c2c80db2e72da82ce3c839
jaejae990921 <jaejae990921@naver.com>
2023-10-12 11:20:30 +0900

    study : typescript event 객체 type 공부
    event 객체의 type 공부 (onClick, onChange 등)

--- React/41_react_typescript/src/Types.tsx
+++ React/41_react_typescript/src/Types.tsx
@@ -18,10 +18,18 @@ const Types: React.FC<Props> = (props) => {
 
   const myInput = useRef<HTMLInputElement>(null);
 
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {};
+
+  const handlefocus = () => {
+    myInput.current!.focus(); // 느낌표는 null이 아니라고 확신할 때 사용, 물음표는 null일 수도 있다고 알려줌
+  };
+
   return (
     <>
       <h2>Hello {props.name}</h2>
       <input ref={myInput} />
+      <button onClick={handlefocus} />
+      <button onClick={(e) => onClick(e)}>버튼</button>
     </>
   );
 };
